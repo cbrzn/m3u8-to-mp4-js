d49dd4967a7d0f11466dca98f43ac641637ceff0
Cesar <cesarbrazon10@gmail.com>
2025-04-29 18:29:24 +0200

    feat: support `startTime` and `duration` in creation

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 /**
  * @description M3U8 to MP4 Converter
  * @author Furkan Inanc
- * @version 1.0.0
+ * @version 1.0.1
  */
 
-let ffmpeg = require("fluent-ffmpeg");
+const ffmpeg = require("fluent-ffmpeg");
 
 /**
  * A class to convert M3U8 to MP4
  * @class
  */
 class m3u8ToMp4Converter {
+  constructor() {
+    this.M3U8_FILE = null;
+    this.OUTPUT_FILE = null;
+    this.START_TIME = null;
+    this.DURATION = null;
+  }
+
   /**
    * Sets the input file
    * @param {String} filename M3U8 file path. You can use remote URL
@@ -19,7 +26,6 @@ class m3u8ToMp4Converter {
   setInputFile(filename) {
     if (!filename) throw new Error("You must specify the M3U8 file address");
     this.M3U8_FILE = filename;
-
     return this;
   }
 
@@ -31,7 +37,32 @@ class m3u8ToMp4Converter {
   setOutputFile(filename) {
     if (!filename) throw new Error("You must specify the file path and name");
     this.OUTPUT_FILE = filename;
+    return this;
+  }
 
+  /**
+   * Sets the start time for the output
+   * @param {String|Number} time Start time in seconds or time format (e.g., "00:01:30")
+   * @returns {Function}
+   */
+  setStartTime(time) {
+    if (time === undefined || time === null) {
+      throw new Error("You must specify a valid start time");
+    }
+    this.START_TIME = time;
+    return this;
+  }
+
+  /**
+   * Sets the duration for the output (alternative to end time)
+   * @param {String|Number} time Duration in seconds or time format (e.g., "30")
+   * @returns {Function}
+   */
+  setDuration(time) {
+    if (time === undefined || time === null) {
+      throw new Error("You must specify a valid duration");
+    }
+    this.DURATION = time;
     return this;
   }
 
@@ -39,36 +70,57 @@ class m3u8ToMp4Converter {
    * Starts the process
    */
   start(options = {}) {
-    // https://github.com/fluent-ffmpeg/node-fluent-ffmpeg#setting-event-handlers
-    options = Object.assign({
-      onStart: () => { },
-      onEnd: () => { },
-      onError: error => {
-        reject(new Error(error));
-      },
-      onProgress: () => { },
-      onStderr: () => { },
-      onCodecData: () => { },
-    }, options);
-    
     return new Promise((resolve, reject) => {
+      options = Object.assign(
+        {
+          onStart: (commandLine) => {
+            console.log("FFmpeg command:", commandLine);
+          },
+          onEnd: () => {},
+          onError: (error) => {
+            reject(new Error(error));
+          },
+          onProgress: () => {},
+          onStderr: (stderr) => {
+            console.log("FFmpeg stderr:", stderr);
+          },
+          onCodecData: (data) => {
+            console.log("Codec data:", data);
+          },
+        },
+        options
+      );
+
       if (!this.M3U8_FILE || !this.OUTPUT_FILE) {
         reject(new Error("You must specify the input and the output files"));
         return;
       }
 
-      ffmpeg(this.M3U8_FILE)
-        .on('start', options.onStart)
-        .on('codecData', options.onCodecData)
-        .on('progress', options.onProgress)
+      const ffmpegCommand = ffmpeg(this.M3U8_FILE);
+
+      if (this.START_TIME) {
+        ffmpegCommand.outputOptions(`-ss ${this.START_TIME}`);
+      }
+
+      if (this.DURATION) {
+        ffmpegCommand.outputOptions(`-t ${this.DURATION}`);
+      }
+
+      ffmpegCommand
+        .on("start", options.onStart)
+        .on("codecData", options.onCodecData)
+        .on("progress", options.onProgress)
         .on("error", options.onError)
-        .on('stderr', options.onStderr)
+        .on("stderr", options.onStderr)
         .on("end", (...args) => {
           resolve();
           options.onEnd(...args);
         })
-        .outputOptions("-c copy")
-        .outputOptions("-bsf:a aac_adtstoasc")
+        .outputOptions("-c:v copy") // Copy video stream
+        .outputOptions("-c:a aac") // Ensure audio is encoded as AAC
+        .outputOptions("-bsf:a aac_adtstoasc") // Bitstream filter for AAC
+        .outputOptions("-map 0") // Map all streams from input
+        .outputOptions("-f mp4") // Explicitly set output format to MP4
         .output(this.OUTPUT_FILE)
         .run();
     });
